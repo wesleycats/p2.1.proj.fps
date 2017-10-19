95178ef1a690fcbf833362086279227680771290
Wesley Cats <23973@ma-web.nl>
2017-10-19 12:14:10 +0200

    Added (test) Map / Level Design + Antidote barriers script
    - MapLevel Design Scene

---
+++ FPS y2 p2.1 GPR/Assets/Scripts/AntidoteBarriers/AntBar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntBar : MonoBehaviour {
+
+    GameObject zombie;
+    Renderer _renderer;
+    Color wallColor;
+    int lives = 100;
+
+    private void Awake()
+    {
+        _renderer = gameObject.GetComponent<Renderer>();
+        wallColor = _renderer.material.color;
+
+        // If zombie tag is different, change the tag, otherwise zet zombie to [Serialized Field]
+        zombie = GameObject.FindGameObjectWithTag("Player");
+    }
+    
+    void Start () {
+    }
+	
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.collider == zombie.GetComponent<CapsuleCollider>())
+        {
+            StartCoroutine(decreaseLife());
+        }
+    }
+
+    private IEnumerator decreaseLife()
+    {
+        lives -= 1;
+        _renderer.material.color = Color.red;
+        yield return new WaitForSeconds(2);
+        _renderer.material.color = wallColor;
+    } 
+}
